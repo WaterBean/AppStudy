69e1739d97f557c71debcb29fff159e0c69c87c5
WaterBean <gkstnqls9@gmail.com>
2024-04-18 16:39:14 +0900

    Setup 1st Project

---
+++ ScaleMaker/ScaleMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScaleMaker
+//
+//  Created by 한수빈 on 4/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
