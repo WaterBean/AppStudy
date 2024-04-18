ff983e8c19ef9f5cb9a470884de8ceb380f3b276
WaterBean <gkstnqls9@gmail.com>
2024-04-18 18:02:34 +0900

    Add Features go home

--- ScaleMaker/ScaleMaker/ViewController.swift
+++ ScaleMaker/ScaleMaker/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblWarnigText: UILabel!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textField.delegate = self
     }
 
-
+    @IBAction func setLabelText(_ sender: Any) {
+        if textField.text == "" {
+            
+        }
+        lblTitle.text = textField.text
+    }
+    
+    @IBAction func playCScale(_ sender: Any) {
+    }
+    
+    
 }
-
+// MARK: TextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+        
+    
+}
